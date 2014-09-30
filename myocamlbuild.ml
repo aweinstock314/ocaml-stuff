b746790c4c8b396ee154db3e99b3351086dcd5af
Avi Weinstock <aweinstock314@gmail.com>
2014-09-30 07:02:48 -0400

    Added preprocessor flag for ocmaldep. Cleaned up add_libs_from_preprocs in preperation of making it recursive.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,8 +4,10 @@ automatically infer libraries to link from "#load"
 directives *)
 
 open Ocamlbuild_plugin;;
-let set_strip_preproc () = 
-    Options.ocaml_cflags := ["-pp"; "../remove_ocaml_toplevel_directives.sh"] @ !Options.ocaml_cflags;;
+let set_strip_preproc () = (
+    Options.ocamldep := S[V"OCAMLDEP";A"-pp"; A"../remove_ocaml_toplevel_directives.sh"];
+    Options.ocaml_cflags := ["-pp"; "../remove_ocaml_toplevel_directives.sh"] @ !Options.ocaml_cflags
+);;
 
 let strip_suffix s = String.sub s 0 (String.rindex s '.');;
 let extract_loadname line =
@@ -22,15 +24,19 @@ let with_each_line_from_file ?bin fname f =
         done with End_of_file -> ()
     );;
 let add_libs_from_preprocs target =
-    let fname = (strip_suffix target) ^ ".ml" in
-    Printf.printf "Source filename is \"%s\"\n%!" fname;
-    with_each_line_from_file fname (fun line ->
-        match extract_loadname line with
-        | Some(x) ->
-            (Options.ocaml_libs @:= [strip_suffix x];
-            Printf.printf "Adding \"%s\" to the libs list.\n%!" x)
-        | None -> ()
-    );;
+    let add_lib libname = (
+        Options.ocaml_libs @:= [strip_suffix x];
+        Printf.printf "Adding \"%s\" to the libs list.\n%!" x
+    ) in
+    let add_file target = 
+        let fname = (strip_suffix target) ^ ".ml" in
+        Printf.printf "Source filename is \"%s\"\n%!" fname;
+        with_each_line_from_file fname (fun line ->
+            match extract_loadname line with
+            | Some(x) -> add_lib x
+            | None -> ()
+        ) in
+    add_file target;;
 
 let strip_preproc_and_inject_libs = (function
 | Before_options -> set_strip_preproc ()
